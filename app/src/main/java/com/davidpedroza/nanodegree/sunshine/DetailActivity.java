e9537b910f8dcd409a17acfe6196ddd6c05aa26d
David <davidpedroza@gmail.com>
2016-08-22 21:18:03 -0300

    Initial commit

---
+++ app/src/main/java/com/davidpedroza/nanodegree/sunshine/DetailActivity.java
@@ -0,0 +1,48 @@
+package com.davidpedroza.nanodegree.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            Bundle bundle = getActivity().getIntent().getExtras();
+            String data = bundle.getString(Intent.EXTRA_TEXT);
+
+            TextView textView = (TextView) rootView.findViewById(R.id.text);
+            textView.setText(data);
+
+            return rootView;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/davidpedroza/nanodegree/sunshine/MainActivity.java
@@ -0,0 +1,44 @@
+package com.davidpedroza.nanodegree.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int menuItemId = item.getItemId();
+
+        switch (menuItemId) {
+            case (R.id.action_settings):
+
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
