ec7e7aaea211e5f2cdbb3b083ee98ff6692fb331
David <davidpedroza@gmail.com>
2016-09-04 22:45:05 -0300

    ContentProvider adicionado ao Manifest

--- app/src/main/java/com/davidpedroza/nanodegree/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/davidpedroza/nanodegree/sunshine/data/WeatherProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.davidpedroza.nanodegree.sunshine;
+package com.davidpedroza.nanodegree.sunshine.data;
 
 import android.annotation.TargetApi;
 import android.content.ContentProvider;
@@ -23,6 +23,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+
+import com.davidpedroza.nanodegree.sunshine.WeatherContract;
+import com.davidpedroza.nanodegree.sunshine.WeatherDbHelper;
 
 public class WeatherProvider extends ContentProvider {
 
@@ -117,14 +120,21 @@ public class WeatherProvider extends ContentProvider {
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
 
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
 
 
         // 3) Return the new matcher!
-        return null;
+        return matcher;
     }
 
     /*
